a0085908c31a0ba6fea7a81d7690f87f4d1e920a
侯雨 <447542807@qq.com>
2024-07-22 23:37:16 +0800

    feat: init路由

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from "vue-router";
+// 导入菜单路由
+
+const router = createRouter({
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(''),
+  routes: [
+    {
+      path: "/",
+      name: "/",
+      children: [
+        {
+          path: "/",
+          name: "DemoOne",
+          component: () => import("@/views/demo1/index.vue"),
+          meta: {
+            title: "官网首页",
+            background: "需要传递的数据",
+          },
+        },
+      ],
+    },
+    // {
+    //   path: "/blank-page-wx-pay",
+    //   name: "BlankPageWxPay",
+    //   meta: {
+    //     title: "课程详情",
+    //   },
+    //   component: () => import("@/views/blank-page-wx-pay/index.vue"),
+    // },
+  ],
+});
+// beforeEach router
+router.beforeEach(async (to, from, next) => {
+  next();
+});
+export default router;
